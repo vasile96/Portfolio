e46670fdb054b7637528a0c3b0aa0deb09c4da23
ursuvasile <vasileursu94@gmail.com>
2023-11-03 16:46:21 +0100

    221

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
- 
-);
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 
 reportWebVitals();
